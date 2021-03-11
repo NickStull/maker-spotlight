d1ba268ad831ec5bfee3a6611027a448461b30fa
Nick Stull <73801944+NickStull@users.noreply.github.com>
2021-03-11 16:41:16 -0600

    adding uid to seeds

--- scripts/usersSeedDB.js
+++ scripts/usersSeedDB.js
@@ -7,24 +7,28 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/makerspotlight"
 
 const usersSeed = [
   {
+    userId: "L2FxlNluqIZVeOA1t5dXU1och6C3",
     firstName: "Nick",
     lastName: "Stull",
     email: "nick@gmail.com"
   },
   {
+    userId: "d28jGwxVdZXYOQGzS6NbxkB3ddP2",
     firstName: "Derek",
     lastName: "Bardini",
-    email: "derek@gmail.com"
+    email: "derekbardini@gmail.com"
   },
   {
+    userId: "u5irgyagz7YT5m77Zsx8Mb51Ngs2",
     firstName: "Tim",
     lastName: "Martin",
-    email: "tim@gmail.com"
+    email: "timmartin13@gmail.com"
   },
   {
+    userId: "x5YnUFqsuZdmBDhPuxUc4bziVoP2",
     firstName: "Hector",
     lastName: "Fernandez",
-    email: "hector@gmail.com"
+    email: "hector@email.com"
   }
 ];
 
