0b8ba2cac5b835ce815d74f4f26902cd4abff467
Hector Fernandez <hkfernandezdev@gmail.com>
2021-03-11 16:23:14 -0600

    commit before pull on main

--- client/src/components/Dropdown/index.js
+++ client/src/components/Dropdown/index.js
@@ -1,27 +1,32 @@
 import React, { useState } from "react";
+import { AuthProvider, useAuth } from '../../utils/contexts/AuthContext'
 import './dropdown.css'
 
 const HeaderDropdown = () => {
 
-	const [dropdown, setDropdownState] = useState("closed");
+  const [dropdown, setDropdownState] = useState("closed");
+  const { currentUser } = useAuth()
 
-	function handleDropdownClick() {
-		dropdown === 'closed' ?
-			setDropdownState('open') :
-			setDropdownState('closed');
-	}
+  function handleDropdownClick() {
+    dropdown === 'closed' ?
+      setDropdownState('open') :
+      setDropdownState('closed');
+    console.log('HEADER USE AUTH', useAuth)
+    console.log('HEADER AUTH PROVIDER', AuthProvider)
+    console.log(currentUser)
+  }
 
-	let userResults = [{ firstName: 'Oscar' }]
+  let userResults = [{ firstName: 'Oscar' }]
 
 
-	return (
-		<div className="dropdown">
-			<span onClick={handleDropdownClick}>Welcome, {userResults.firstName} you rascal</span>
-			{/* <div className={dropdown === 'closed' ? "hide" : "dropdown-content"}> */}
-			<div className={dropdown === 'closed' ? "hide" : "dropdown-content"}>Hello World! Additional Content</div>
-			{/* </div> */}
-		</div>
-	)
+  return (
+    <div className="dropdown">
+      <span onClick={handleDropdownClick}>Welcome, {userResults.firstName} you rascal</span>
+      {/* <div className={dropdown === 'closed' ? "hide" : "dropdown-content"}> */}
+      <div className={dropdown === 'closed' ? "hide" : "dropdown-content"}>Hello World! Additional Content</div>
+      {/* </div> */}
+    </div>
+  )
 }
 
 export default HeaderDropdown;
