d83a51cf57113449121f12a2d85c8a8c0a3e4688
Hector Fernandez <hkfernandezdev@gmail.com>
2021-03-11 10:29:15 -0600

    commit before pull request

--- client/src/components/Signup/index.js
+++ client/src/components/Signup/index.js
@@ -1,190 +1,89 @@
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-import React from 'react'
-import ReactDom from 'react-dom'
-=======
-=======
->>>>>>> 151fdd012172179b2d6cf9b0ccc31c6c67c4b90a
 import React, { useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../../utils/contexts/AuthContext';
 import Home from '../Home';
-<<<<<<< HEAD
->>>>>>> Stashed changes
-=======
->>>>>>> 151fdd012172179b2d6cf9b0ccc31c6c67c4b90a
-
 const styles = {
-  modal: {
-    position: 'fixed',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    backgroundColor: '#FFF',
-    padding: '50px',
-    zIndex: 1000
-  },
-  overlay: {
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    backgroundColor: 'rgba(0, 0, 0, .7)',
-    zIndex: 1000
-  }
+	modal: {
+		position: 'fixed',
+		top: '50%',
+		left: '50%',
+		transform: 'translate(-50%, -50%)',
+		backgroundColor: '#FFF',
+		padding: '50px',
+		zIndex: 1000
+	},
+	overlay: {
+		position: 'fixed',
+		top: 0,
+		left: 0,
+		right: 0,
+		bottom: 0,
+		backgroundColor: 'rgba(0, 0, 0, .7)',
+		zIndex: 1000
+	}
 }
-
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-const Signup = ({ open, onClose }) => {
-  if (!open) return null
-=======
 const Signup = () => {
-  
-  const emailRef = useRef();
-  const passwordRef = useRef();
-  const passwordConfirmRef = useRef();
-  const { signup } = useAuth()
-  const [error, setError] = useState('')
-  const [loading, setLoading] = useState(false)
->>>>>>> 151fdd012172179b2d6cf9b0ccc31c6c67c4b90a
-
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-
-    if(passwordRef.current.value !== passwordConfirmRef.current.value) {
-      return setError('Passwords do not match')
-    }
-
-    try {
-      setError('')
-      setLoading(true)
-      await signup(emailRef.current.value, passwordRef.current.value)
-    } catch {
-      setError('Failed to create an account')
-    }
-
-    setLoading(false)
-  }
-
-  return (
-    <>
-      <Home />
-      <div style={styles.overlay} />
-      <div style={styles.modal}>
-        <button>
-          <Link to='/'>
-            Close Modal
+	const emailRef = useRef();
+	const passwordRef = useRef();
+	const passwordConfirmRef = useRef();
+	const { signup } = useAuth()
+	const [error, setError] = useState('')
+	const [loading, setLoading] = useState(false)
+	const handleSubmit = async (e) => {
+		e.preventDefault()
+		if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+			return setError('Passwords do not match')
+		}
+		try {
+			setError('')
+			setLoading(true)
+			await signup(emailRef.current.value, passwordRef.current.value)
+		} catch {
+			setError('Failed to create an account')
+		}
+		setLoading(false)
+	}
+	return (
+		<>
+			<Home />
+			<div style={styles.overlay} />
+			<div style={styles.modal}>
+				<button>
+					<Link to='/'>
+						Close Modal
           </Link>
-        </button>
-        {error && <div>{error}</div>}
-        <form className="form" onSubmit={handleSubmit}>
-          <input
-            name="email"
-            type="email"
-            placeholder="Email"
-            ref={emailRef}
-            required
-          />
-          <input
-            name="password"
-            type="password"
-            placeholder="Password"
-            ref={passwordRef}
-            required
-          />
-          <input
-            name="passwordConfirm"
-            type="password"
-            placeholder="Confirm Password"
-            ref={passwordConfirmRef}
-            required
-          />
-          <button 
-            disabled={loading}
-            type="submit">
-              Submit
+				</button>
+				{error && <div>{error}</div>}
+				<form className="form" onSubmit={handleSubmit}>
+					<input
+						name="email"
+						type="email"
+						placeholder="Email"
+						ref={emailRef}
+						required
+					/>
+					<input
+						name="password"
+						type="password"
+						placeholder="Password"
+						ref={passwordRef}
+						required
+					/>
+					<input
+						name="passwordConfirm"
+						type="password"
+						placeholder="Confirm Password"
+						ref={passwordConfirmRef}
+						required
+					/>
+					<button
+						disabled={loading}
+						type="submit">
+						Submit
             </button>
-        </form>
-      </div>
-<<<<<<< HEAD
-    </>,
-    document.getElementById('portal')
-=======
-const Signup = () => {
-  
-  const emailRef = useRef();
-  const passwordRef = useRef();
-  const passwordConfirmRef = useRef();
-  const { signup } = useAuth()
-  const [error, setError] = useState('')
-  const [loading, setLoading] = useState(false)
-
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-
-    if(passwordRef.current.value !== passwordConfirmRef.current.value) {
-      return setError('Passwords do not match')
-    }
-
-    try {
-      setError('')
-      setLoading(true)
-      await signup(emailRef.current.value, passwordRef.current.value)
-    } catch {
-      setError('Failed to create an account')
-    }
-
-    setLoading(false)
-  }
-
-  return (
-    <>
-      <Home />
-      <div style={styles.overlay} />
-      <div style={styles.modal}>
-        <button>
-          <Link to='/'>
-            Close Modal
-          </Link>
-        </button>
-        {error && <div>{error}</div>}
-        <form className="form" onSubmit={handleSubmit}>
-          <input
-            name="email"
-            type="email"
-            placeholder="Email"
-            ref={emailRef}
-            required
-          />
-          <input
-            name="password"
-            type="password"
-            placeholder="Password"
-            ref={passwordRef}
-            required
-          />
-          <input
-            name="passwordConfirm"
-            type="password"
-            placeholder="Confirm Password"
-            ref={passwordConfirmRef}
-            required
-          />
-          <button 
-            disabled={loading}
-            type="submit">
-              Submit
-            </button>
-        </form>
-      </div>
-    </>
->>>>>>> Stashed changes
-=======
-    </>
->>>>>>> 151fdd012172179b2d6cf9b0ccc31c6c67c4b90a
-  )
+				</form>
+			</div>
+		</>
+	)
 }
-
 export default Signup;
--- client/src/components/SignupButton/index.js
+++ client/src/components/SignupButton/index.js
@@ -1,41 +1,14 @@
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-import { useState } from 'react'
-import Signup from '../Signup'
-=======
 import React from 'react';
 import { Link } from 'react-router-dom';
->>>>>>> 151fdd012172179b2d6cf9b0ccc31c6c67c4b90a
-
 const SignupButton = () => {
-
-  return (
-<<<<<<< HEAD
-    <div>
-      <button onClick={() => setIsOpen(true)}>Sign Up</button>
-      <Signup open={isOpen} onClose={() => setIsOpen(false)}/>
-    </div>
-=======
-import React from 'react';
-import { Link } from 'react-router-dom';
-
-const SignupButton = () => {
-
-  return (
-=======
->>>>>>> 151fdd012172179b2d6cf9b0ccc31c6c67c4b90a
-      <div>
-        <button>
-          <Link to='/signup'>
-            Sign Up
+	return (
+		<div>
+			<button>
+				<Link to='/signup'>
+					Sign Up
           </Link>
-        </button>
-      </div>
-<<<<<<< HEAD
->>>>>>> Stashed changes
-=======
->>>>>>> 151fdd012172179b2d6cf9b0ccc31c6c67c4b90a
-  )
+			</button>
+		</div>
+	)
 }
-
 export default SignupButton;
