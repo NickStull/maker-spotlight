8ff54c2d239ac87739961056bfd4ec360363550b
Nick Stull <73801944+NickStull@users.noreply.github.com>
2021-03-10 22:29:37 -0600

    adding sign up form with link to firebase

--- client/src/components/SignupButton/index.js
+++ client/src/components/SignupButton/index.js
@@ -4,10 +4,10 @@ import Signup from '../Signup'
 const SignupButton = () => {
   const [isOpen, setIsOpen] = useState(false)
   return (
-    <div>
-      <button onClick={() => setIsOpen(true)}>Sign Up</button>
-      <Signup open={isOpen} onClose={() => setIsOpen(false)}/>
-    </div>
+      <div>
+        <button onClick={() => setIsOpen(true)}>Sign Up</button>
+        <Signup open={isOpen} onClose={() => setIsOpen(false)}/>
+      </div>
   )
 }
 
