8e947ff0765ef823c75868f50e02dd6b7db1c673
Bardini <derekbardini@gmail.com>
2021-03-11 18:30:05 -0600

    added variable query to imgur Controller

--- controllers/imgurController.js
+++ controllers/imgurController.js
@@ -1,10 +1,10 @@
 // const db = require("../models");
 const axios = require("axios");
-
+let query = "W1DJ2DE";
 module.exports = {
   imgur: function (req, res) {
     axios
-      .get("https://api.imgur.com/3/image/W1DJ2DE", {
+      .get("https://api.imgur.com/3/image/" + query, {
         headers: {
           Authorization: `Client-ID 667bcb070bc5aa9`,
         },
