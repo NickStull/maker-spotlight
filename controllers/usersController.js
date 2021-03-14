f3d56a5a7c3996c48bc4daa36cd1572a91c13199
Bardini <derekbardini@gmail.com>
2021-03-14 17:32:13 -0500

    makers route working

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -57,4 +57,11 @@ module.exports = {
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
+
+  findMakers: function (req, res) {
+    console.log("A backend maker");
+    db.Users.find({ wantTo: true })
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
 };
