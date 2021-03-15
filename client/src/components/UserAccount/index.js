6708b506eb1bb570b42497c09daea73f698db1d7
Nick Stull <73801944+NickStull@users.noreply.github.com>
2021-03-14 21:05:51 -0500

    even more bug fixes

--- client/src/components/UserAccount/index.js
+++ client/src/components/UserAccount/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Form, Button, Card } from 'react-bootstrap';
+import { Form, Button, Card, Container } from 'react-bootstrap';
 import { useAuth } from '../../utils/contexts/AuthContext';
 import API from "../../utils/API"
 
@@ -8,7 +8,9 @@ const UserAccount = () => {
   const [user, setUser] = useState();
 
   useEffect(() => {
+    console.log("use effect")
     if (currentUser) {
+      console.log("in if")
       getUser();
     }
   }, []);
@@ -26,54 +28,61 @@ const UserAccount = () => {
 
   return (
     <>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">Edit Account Info</h2>
+      {user  
+        ?<Container className="d-flex mt-4 justify-content-center" style={{ minHeight: "100vh" }} >
+          <div className="w-100" style={{ maxWidth: "400px" }}>
+            <Card>
+              <Card.Body>
+                <h2 className="text-center mb-4">Edit Account Info</h2>
 
-          <Form className="form">
-            <Form.Group controlId="formBasicFirst">
-              <Form.Label>First Name</Form.Label>
-              <Form.Control 
-                name="firstName"
-                type="text" 
-                defaultValue={ user.firstName } 
-                // ref={firstNameRef}
-                required
-              />
-            </Form.Group>
-            <Form.Group controlId="formBasicLast">
-              <Form.Label>Last Name</Form.Label>
-              <Form.Control 
-                name="lastName"
-                type="text" 
-                value={ user.lastName }  
-                // ref={lastNameRef}
-                required
-              />
-            </Form.Group>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>Email</Form.Label>
-              <Form.Control 
-                name="email"
-                type="email" 
-                value={ currentUser.email } 
-                // ref={emailRef}
-                required
-              />
-            </Form.Group>
-            <Form.Group >
-              <Form.Check type="switch"
-                          label="Wants to be featured" 
-                          id="want-to-switch"
-                          // defaultChecked={ props.user.data.wantTo } 
-              />
-            </Form.Group>
-            <Button className="w-100" variant="primary" type="submit">
-              Save
-            </Button>
-          </Form>
-        </Card.Body>
-      </Card>
+                <Form className="form">
+                  <Form.Group controlId="formBasicFirst">
+                    <Form.Label>First Name</Form.Label>
+                    <Form.Control 
+                      name="firstName"
+                      type="text" 
+                      defaultValue={ user.firstName } 
+                      // ref={firstNameRef}
+                      required
+                    />
+                  </Form.Group>
+                  <Form.Group controlId="formBasicLast">
+                    <Form.Label>Last Name</Form.Label>
+                    <Form.Control 
+                      name="lastName"
+                      type="text" 
+                      value={ user.lastName }  
+                      // ref={lastNameRef}
+                      required
+                    />
+                  </Form.Group>
+                  <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control 
+                      name="email"
+                      type="email" 
+                      value={ currentUser.email } 
+                      // ref={emailRef}
+                      required
+                    />
+                  </Form.Group>
+                  <Form.Group >
+                    <Form.Check type="switch"
+                                label="Wants to be featured" 
+                                id="want-to-switch"
+                                // defaultChecked={ props.user.data.wantTo } 
+                    />
+                  </Form.Group>
+                  <Button className="w-100" variant="primary" type="submit">
+                    Save
+                  </Button>
+                </Form>
+              </Card.Body>
+            </Card>
+          </div>
+        </Container>
+        : <></>
+      } 
     </>
   )
 }
