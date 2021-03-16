427230ccd7bef9af7e8359e84ebaf53829c76bc0
Tim Martin <timmartin13@gmail.com>
2021-03-16 12:22:47 -0500

    saving before merge

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import CarouselViewer from '../Carousel';
 import MakerBio from '../MakerBio';
+import MakerInfo from '../MakerInfo';
 
 const Home = () => {
   return (
     <>
       <MakerBio />
       <CarouselViewer />
+      <MakerInfo />
     </>
   )
 }
