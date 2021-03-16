950038bd61e1f88ae7fe743a81ef4539f40ae343
Bardini <derekbardini@gmail.com>
2021-03-16 15:10:43 -0500

    now must be logged in to view account and vote pages

--- client/src/components/PrivateRoutes/PrivateAdminRoute.js
+++ client/src/components/PrivateRoutes/PrivateAdminRoute.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Redirect, Route } from "react-router-dom";
-import { useAuth, sendUserInfo } from "../utils/contexts/AuthContext";
-import Login from "./Login";
+import { useAuth, sendUserInfo } from "../../utils/contexts/AuthContext";
+import Login from "../Login";
 
-export default function PrivateRoute({ component: Component, ...rest }) {
+export default function PrivateAdminRoute({ component: Component, ...rest }) {
   const { currentUser, userInfo } = useAuth();
   return (
     <Route
