edcdc1ec12b02243cc3e2917b9211a37b292d077
Tim Martin <timmartin13@gmail.com>
2021-03-16 19:13:03 -0500

    hide/show working when users are logged in

--- client/src/components/Carousel/index.js
+++ client/src/components/Carousel/index.js
@@ -6,7 +6,7 @@ import './carousel.css'
 
 const CarouselViewer = () => {
 
-	const { newsletterInfo } = useAuth()
+	const { newsletterInfo, currentUser } = useAuth()
 	const [indexState, setIndexState] = useState(0);
 
 	// handles the advancing of the slides
@@ -17,29 +17,32 @@ const CarouselViewer = () => {
 	return (
 
 		<Container fluid className='containerFluid'>
-			<Row className='carouselWrapper'>
-				<Carousel activeIndex={indexState} onSelect={handleSelect}>
-					{newsletterInfo.photos.map(
-						(image, indexState) => {
-							return (
-								<Carousel.Item key={indexState}>
-									<CloudinaryContext cloudName="makerspotlight">
-										<Image
-											publicId={image.link}
-											className="d-block w-100 carouselImg"
-											alt="First slide"
-											background="auto" crop="pad"
-										/>
-									</CloudinaryContext>
-									<figcaption className='carouselCaption'>
-										{image.description}
-									</figcaption>
-								</Carousel.Item>
-							)
-						}
-					)}
-				</Carousel>
-			</Row>
+      { currentUser ?
+        <Row className='carouselWrapper'>
+          <Carousel activeIndex={indexState} onSelect={handleSelect}>
+            {newsletterInfo.photos.map(
+              (image, indexState) => {
+                return (
+                  <Carousel.Item key={indexState}>
+                    <CloudinaryContext cloudName="makerspotlight">
+                      <Image
+                        publicId={image.link}
+                        className="d-block w-100 carouselImg"
+                        alt="First slide"
+                        background="auto" crop="pad"
+                      />
+                    </CloudinaryContext>
+                    <figcaption className='carouselCaption'>
+                      {image.description}
+                    </figcaption>
+                  </Carousel.Item>
+                )
+              }
+            )}
+          </Carousel>
+        </Row>
+        : <> </>
+      }
 		</Container>
 
 	)
