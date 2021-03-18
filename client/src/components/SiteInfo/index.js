639586ad7ec1666f2c6243a69e0dc14bee7a96b8
Tim Martin <timmartin13@gmail.com>
2021-03-18 13:16:58 -0500

    fixing logo when logged out

--- client/src/components/SiteInfo/index.js
+++ client/src/components/SiteInfo/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Container, Card, Row, Col } from 'react-bootstrap';
 import { useAuth } from '../../utils/contexts/AuthContext';
+import "./style.css";
 
 function SiteInfo() {
   
@@ -11,10 +12,10 @@ function SiteInfo() {
       { !currentUser ?
         <Card style={{ width: "700px"}}>
           <div className="row no-gutters">
-            <Card.Body className="cardBody">
+            <Card.Body className="cardBody" style={{ maxWidth: "700px", overflowY: "auto"}}>
               <Row style={{ alignItems: "center" }}>
                 <Col style={{ textAlign: "center" }}>
-                  <img style={{ maxWidth: "250px"}} src={ newsletterInfo.bioPicture } alt="maker bio" />
+                  <img className="bioPicture" src={ newsletterInfo.bioPicture } alt="maker bio" />
                   <h5 className="card-title" style={{ fontSize: "30px", marginTop: "10px" }}>Featured: { newsletterInfo.firstName + " " + newsletterInfo.lastName }</h5>
                   <div style={{ marginTop: "40px" }}>
                     <p>Featured Edge aims to connect knife artists with enthusiasts of knife artistry.</p>
